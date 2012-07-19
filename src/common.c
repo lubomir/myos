b798be32eba595422b04db12e80395d9f9b4018e
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-07-19 23:39:43 +0200

    Add implementation of some standard functions
     - memset
     - memcpy
     - strcat
     - strcpy
     - strcmp

--- src/common.c
+++ src/common.c
@@ -22,3 +22,61 @@ u16int inw(u16int port)
     u16int ret;
     asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
 }
+
+void * memset(u8int *dest, u8int val, u32int len)
+{
+    u8int *tmp = dest;
+    for ( ; len > 0; --len) *tmp++ = val;
+    return dest;
+}
+
+void * memcpy(u8int *dest, const u8int *src, u32int len)
+{
+    u8int *tmp = dest;
+    for ( ; len > 0; --len) *tmp++ = *src++;
+    return dest;
+}
+
+int strcmp(const char *str1, const char *str2)
+{
+    while (*str1 && *str2) {
+        if (*str1 < *str2) {
+            return -1;
+        }
+        if (*str1 > *str2) {
+            return 1;
+        }
+        str1++;
+        str2++;
+    }
+    if (!*str1 && *str2) {
+        return -1; /* str1 is longer */
+    }
+    if (*str1 && !*str2) {
+        return 1; /* str2 is longer */
+    }
+    return 0;
+}
+
+char * strcpy(char *dest, const char *src)
+{
+    char *dp = dest;
+    do {
+        *dp++ = *src++;
+    } while (*src);
+    return dest;
+}
+
+char * strcat(char *dest, const char *src)
+{
+    char *dp = dest;
+    while (*dp) {
+        dp++;
+    }
+
+    do {
+        *dp++ = *src++;
+    } while (*src);
+
+    return dest;
+}
