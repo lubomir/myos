17acc1581c740dbe14907a628f21e7d66339a836
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-07-20 15:47:04 +0200

    Clean up main function
    Some debug code was removed.

--- src/main.c
+++ src/main.c
@@ -7,44 +7,11 @@
 #include "common.h"
 #include "timer.h"
 
-#define CHECK_INT(TYPE, SIZE) do {              \
-    if (sizeof(TYPE) != SIZE) {                 \
-        monitor_write("PROBLEM: ");             \
-        monitor_write(#TYPE " has size ");      \
-        monitor_write_dec(sizeof(TYPE));        \
-        monitor_write(", "#SIZE" expected\n");  \
-    } } while (0)
-
-static void check_types(void)
-{
-    monitor_write("Checking types...\n");
-    CHECK_INT(u32int, 4);
-    CHECK_INT(u16int, 2);
-    CHECK_INT(u8int, 1);
-    CHECK_INT(s32int, 4);
-    CHECK_INT(s16int, 2);
-    CHECK_INT(s8int, 1);
-}
 
 int main(struct multiboot *mboot_ptr)
 {
     init_descriptor_tables ();
     monitor_clear();
-    check_types();
-    monitor_write("Hello, world!\n");
-    monitor_write("Decimal number: ");
-    monitor_write_dec(12345);
-    monitor_write(" ");
-    monitor_write_dec(0);
-    monitor_write(" ");
-    monitor_write_dec(10101010);
-    monitor_write("\nHexa number: ");
-    monitor_write_hex(0xDEADCAFE);
-    monitor_write(" ");
-    monitor_write_hex(0x0);
-    monitor_write(" ");
-    monitor_write_hex(0x10101010);
-    monitor_write("\n");
 
     asm volatile ("int $0x3");
     asm volatile ("int $0x4");
