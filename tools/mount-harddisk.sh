a6d0357423c52285ad264e9ad9e95bbb4570f147
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-07-28 23:04:54 +0200

    Add script to mount the harddisk image
    So far the script mounts one partition that must start at sector 63.

---
+++ tools/mount-harddisk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+IMG=harddisk
+DIR=/mnt/my-system
+
+sudo mount $IMG $DIR -o uid=$(id -u),gid=$(id -g),loop,offset=$((63 * 512))
+pushd $DIR >/dev/null
+printf "When finished, type 'exit' to unmount.\n"
+bash
+popd >/dev/null
+sudo umount $DIR
