5a174080693eeba810aac7adff24d230b6c06dbc
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-08-07 18:14:51 +0200

    Make structures for initrd private

--- src/initrd.h
+++ src/initrd.h
@@ -12,23 +12,6 @@
 #include "common.h"
 #include "fs.h"
 
-/** Header of initial ramdisk. */
-typedef struct {
-    u32int nfiles;  /**< Number of files stored in the ramdisk. */
-} initrd_header_t;
-
-/** Header for individual file on ramdisk. */
-typedef struct {
-    /** Magic number for error checking. */
-    u8int magic;
-    /** Filename. */
-    u8int name[64];
-    /** Offset in the initrd where the file starts. */
-    u32int offset;
-    /** Length of the file. */
-    u32int length;
-} initrd_file_header_t;
-
 /**
  * Initialises the initial ramdisk.
  *
