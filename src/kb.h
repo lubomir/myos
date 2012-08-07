81910be9d6db92ac43faa694366cf7c9f60989d8
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-08-07 17:22:29 +0200

    Document src/kb.h

--- src/kb.h
+++ src/kb.h
@@ -1,5 +1,8 @@
-/*
- * kb.h -- Defines functions for and structures relating to keyboard.
+/**
+ * @file    kb.h
+ *
+ * Defines functions for and structures relating to keyboard.
+ *
  * Adapted from Bran's kernel development tutorial.
  */
 
@@ -8,20 +11,23 @@
 
 #include "common.h"
 
-/* Current state of the keyboard - pressed modifier keys, state of lights.
- * These macros are used to access it. */
-#define KB_STATE_CTRL       0x01
-#define KB_STATE_ALT        0x02
-#define KB_STATE_SHIFT      0x04
-#define KB_STATE_SCRLOCK    0x08
-#define KB_STATE_NUMLOCK    0x10
-#define KB_STATE_CAPSLOCK   0x20
+/* Current state of the keyboard - pressed modifier keys, state of lights. */
+#define KB_STATE_CTRL       0x01    /**< Control key pressed */
+#define KB_STATE_ALT        0x02    /**< Alt key pressed */
+#define KB_STATE_SHIFT      0x04    /**< Shift key pressed */
+#define KB_STATE_SCRLOCK    0x08    /**< Scroll lock key pressed */
+#define KB_STATE_NUMLOCK    0x10    /**< Num lock key pressed */
+#define KB_STATE_CAPSLOCK   0x20    /**< Caps lock key pressed */
 
-/* Variable defined in kb.c holding the state of keyboard. */
+/** Variable defined in src/kb.c holding the state of keyboard.
+ * Use KB_STATE_* macros to access it. */
 extern u8int kb_state;
 
-/*
+/**
  * Register the keyboard interrupt handler with given keyboard map.
+ * For description of keyboard map format, see docs/keymap.txt.
+ *
+ * @param map   keybord map to use
  */
 void initialise_keyboard(u8int *map);
 
