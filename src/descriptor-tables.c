3ed3d97cbd80f7fce0df3da87b7ead89f568788a
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-08-07 19:34:53 +0200

    Make GDT and IDT much less visible
    This way it is reasonable to remove the typedefs.

--- src/descriptor-tables.c
+++ src/descriptor-tables.c
@@ -11,6 +11,91 @@
 #include "isr.h"
 
 /**
+ * This structure contains the value of one GDT entry. We use the attribute
+ * 'packed' to tell GCC not to change any of the alignment in the structure.
+ */
+struct gdt_entry_struct {
+    /** The lower 16 bits of the limit */
+    u16int limit_low;
+    /** The lower 16 bits of the base */
+    u16int base_low;
+    /** The next 8 bits of the base */
+    u8int base_middle;
+    /** Access flags, determine what ring this segment can be used in.
+     * Format:
+     *
+     *      | 7 | 6 5 |  4 | 3  0 |
+     *      | P | DPL | DT | Type |
+     *
+     * + P    - is segment present? (1 -> Yes)
+     * + DPL  - descriptor privilege level - Ring 0-3
+     * + DT   - descriptor type
+     * + Type - segment type - code/data segment
+     *
+     */
+    u8int access;
+    /** Granularity of the segment.
+     * Format:
+     *
+     *      | 7 | 6 | 5 | 4 | 3 0 |
+     *      | G | D | 0 | A | Len |
+     *
+     * + G    - granularity (0 = 1 B, 1 = 1 kB)
+     * + D    - operand size (0 = 16 b, 1 = 32 b)
+     * + 0    - should always be zero
+     * + A    - available for system use (always zero)
+     */
+    u8int granularity;
+    /** The last 8 bits of the base */
+    u8int base_high;
+} PACKED;
+
+/**
+ * This is a pointer to table of gdt_entry_struct's.
+ */
+struct gdt_ptr_struct {
+    /** The upper 16 bits of all selector limits */
+    u16int limit;
+    /** The address of the first gdt_entry_t struct */
+    u32int base;
+} PACKED;
+
+/**
+ * A struct describing an interrupt gate.
+ */
+struct idt_entry_struct {
+    /** The lower 16 bits of the address to jump to when this interrupt fires */
+    u16int base_lo;
+    /** Kernel segment selector */
+    u16int sel;
+    /** This must always be zero */
+    u8int always0;
+    /** More flags.
+     * Format:
+     *
+     *      | 7 | 6 5 | 4   0 |
+     *      | P | DPL | 00110 |
+     *
+     * P    - is entry present? (0 will cause "Interrupt Not Handled")
+     * DPL  - privilege we expect to be called from
+     * The lower 5 bits should always be 0b0110 - 14 in decimal
+     */
+    u8int flags;
+    /** The upper 16 bits of the address to jump to */
+    u16int base_hi;
+} PACKED;
+
+/**
+ * A struct describing a pointer to an array of interrupt handlers.
+ * This is in a format suitable for giving to 'lidt'.
+ */
+struct idt_ptr_struct {
+    u16int limit;
+    /** The address of the first element in our idt_entry_t array */
+    u32int base;
+} PACKED;
+
+/**
  * A struct describing a Task State Segment.
  */
 struct tss_entry_struct {
@@ -67,10 +152,10 @@ static void init_idt(void);
 static void idt_set_gate(u8int, u32int, u16int, u8int);
 static void write_tss(s32int, u16int, u32int);
 
-gdt_entry_t gdt_entries[6];
-gdt_ptr_t   gdt_ptr;
-idt_entry_t idt_entries[256];
-idt_ptr_t   idt_ptr;
+struct gdt_entry_struct gdt_entries[6];
+struct gdt_ptr_struct   gdt_ptr;
+struct idt_entry_struct idt_entries[256];
+struct idt_ptr_struct   idt_ptr;
 struct tss_entry_struct tss_entry;
 
 extern isr_t interrupt_handlers[];
@@ -216,10 +301,10 @@ static void gdt_set_gate(s32int num,
 
 static void init_idt(void)
 {
-    idt_ptr.limit = sizeof(idt_entry_t) * 256 - 1;
+    idt_ptr.limit = sizeof(struct idt_entry_struct) * 256 - 1;
     idt_ptr.base  = (u32int) &idt_entries;
 
-    memset(&idt_entries, 0, sizeof(idt_entry_t) * 256);
+    memset(&idt_entries, 0, sizeof(struct idt_entry_struct) * 256);
 
     /* Remap the irq table */
     outb(PIC1, ICW1_ICW4 | ICW1_INIT); /* Start init sequence */
