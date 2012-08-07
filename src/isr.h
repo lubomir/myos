821ddaa33d9c1d4786479cfbb349e302e7b9a531
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2012-08-07 17:18:43 +0200

    Document src/isr.h

--- src/isr.h
+++ src/isr.h
@@ -1,5 +1,8 @@
-/*
- * isr.h -- Interface and structures for high level interrupt service routines
+/**
+ * @file    isr.h
+ *
+ * Interface and structures for high level interrupt service routines
+ *
  * Part of this code is modified from Bran's kernel development tutorials.
  * Rewritten for and taken from JamesM's kernel development tutorials.
  */
@@ -9,14 +12,20 @@
 
 #include "common.h"
 
+/**
+ * This structure represents contents of registers.
+ * It is passed to interrupt handlers.
+ */
 typedef struct registers {
-    /* Data segment selector */
+    /** Data segment selector. */
     u32int ds;
     /* Pushed by pusha */
     u32int edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    /* Interrupt number and error code (if applicable) */
-    u32int int_no, err_code;
-    /* Pushed by the processor automatically */
+    /** Interrupt number. */
+    u32int int_no;
+    /** Error code (if applicable). */
+    u32int err_code;
+    /* Pushed by the processor automatically. */
     u32int eip, cs, eflags, useresp, ss;
 } registers_t;
 
@@ -37,11 +46,17 @@ typedef struct registers {
 #define IRQ14   46
 #define IRQ15   47
 
-/*
+/**
  * Enables registration of callbacks for interrupts and IRQs.
  * For IRQs, to ease the confusion, use #defines above as the first parameter.
  */
 typedef void (*isr_t)(registers_t*);
+
+/**
+ * Set a handler for given interrupt.
+ * @param n         interrupt number
+ * @param handler   function to be executed for that interrupt
+ */
 void register_interrupt_handler(u8int n, isr_t handler);
 
 #endif /* end of include guard: ISR_H */
